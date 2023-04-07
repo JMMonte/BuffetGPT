a4264f941b1f65bd5268d511106549cfa388c071
JMMonte <joaommontenegro1@gmail.com>
2023-04-07 01:18:17 +0100

    Removed hardcoded values in strategies

--- strategies.py
+++ strategies.py
@@ -3,7 +3,7 @@ import numpy as np
 from ta.momentum import RSIIndicator
 from ta.volatility import BollingerBands
 
-def moving_average_strategy(df, short_window=10, long_window=30):
+def moving_average_strategy(df, short_window, long_window):
     # Calculate the moving averages
     df['Short_MA'] = df['Close'].rolling(window=short_window).mean()
     df['Long_MA'] = df['Close'].rolling(window=long_window).mean()
@@ -12,7 +12,7 @@ def moving_average_strategy(df, short_window=10, long_window=30):
     df['Buy'] = (df['Short_MA'] > df['Long_MA']) & (df['Short_MA'].shift(1) <= df['Long_MA'].shift(1))
     df['Sell'] = (df['Short_MA'] < df['Long_MA']) & (df['Short_MA'].shift(1) >= df['Long_MA'].shift(1))
 
-def momentum_strategy(df, rsi_period=14, rsi_min=30, rsi_max=70):
+def momentum_strategy(df, rsi_period, rsi_min, rsi_max):
     # Calculate RSI
     rsi = RSIIndicator(df['Close'], rsi_period)
     df['RSI'] = rsi.rsi()
@@ -21,7 +21,7 @@ def momentum_strategy(df, rsi_period=14, rsi_min=30, rsi_max=70):
     df['Buy'] = (df['RSI'] < rsi_min)
     df['Sell'] = (df['RSI'] > rsi_max)
 
-def bollinger_bands_strategy(df, window=20, num_of_std=2):
+def bollinger_bands_strategy(df, window, num_of_std):
     # Calculate Bollinger Bands
     bollinger = BollingerBands(df['Close'], window, num_of_std)
     df['Upper_BB'] = bollinger.bollinger_hband()
